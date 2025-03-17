d856244e51e684e1c538cce3bd525bb8c6261be2
rakor2 <winster2207@gmail.com>
2025-03-17 11:53:36 +0300

    ignore readme

--- Mods/ScriptExtender/README.lua
+++ Mods/ScriptExtender/README.lua
@@ -1,1 +1,1 @@
-excuse = "The code was mostly written by Claude-sonnet,  don't expect it to be consistent and optimized :)))))))" -- author's excuse _ai
+excuse = "The code was mostly written by Claude-sonnet,  don't expect it to be consistent and optimized :))))))). If a function has _ai comment, it was writte by Claude" -- author's excuse _ai
